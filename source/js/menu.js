4aa75c88bb60e4296ebbdc78e4869ce323881b89
ilishenko <60015069+ilishenko@users.noreply.github.com>
2020-04-16 00:23:59 +0300

    Добавляет меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,9 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var color = document.querySelector('.page-header');
 
+navMain.classList.remove('main-nav--nojs');
+color.classList.remove('page-header--nojs');
+
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
